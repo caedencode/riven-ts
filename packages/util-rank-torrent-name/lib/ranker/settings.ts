3691de0c9fcf41589edc221fc9274bd45a680943
Josh <joshuav2012@live.co.uk>
2026-06-01 22:55:03 +0100

    feat(core): extract hard coded ranking (#169)
    Co-authored-by: Joe McKie <4980618+joemckie@users.noreply.github.com>

--- packages/util-rank-torrent-name/lib/ranker/settings.ts
+++ packages/util-rank-torrent-name/lib/ranker/settings.ts
@@ -496,7 +496,7 @@ export function getCustomRank(
   return cat[key as keyof typeof cat];
 }
 
-export const RankingModelSchema = z.object({
+export const RankingModelSchema = z.strictObject({
   // Quality
   av1: z.int().default(0),
   avc: z.int().default(0),
@@ -577,72 +577,6 @@ export const RankingModelSchema = z.object({
 
 export type RankingModel = z.infer<typeof RankingModelSchema>;
 
-export const createRankingModel = (input: z.input<typeof RankingModelSchema>) =>
-  RankingModelSchema.decode(input);
-
-export const defaultRankingModel = createRankingModel({
-  // Quality
-  av1: 500,
-  avc: 500,
-  bluray: 100,
-  dvd: -5000,
-  hdtv: -5000,
-  hevc: 500,
-  mpeg: -1000,
-  remux: 10000,
-  vhs: -10000,
-  web: 100,
-  webdl: 200,
-  webmux: -10000,
-  xvid: -10000,
-  pdtv: -10000,
-
-  // Rips
-  bdrip: -5000,
-  brrip: -10000,
-  dvdrip: -5000,
-  hdrip: -10000,
-  ppvrip: -10000,
-  tvrip: -10000,
-  uhdrip: -5000,
-  vhsrip: -10000,
-  webdlrip: -10000,
-  webrip: -1000,
-
-  // HDR
-  bit10: 100,
-  dolbyVision: 3000,
-  hdr: 2000,
-  hdr10plus: 2100,
-
-  // Audio
-  aac: 100,
-  atmos: 1000,
-  dolbyDigital: 50,
-  dolbyDigitalPlus: 150,
-  dtsLossy: 100,
-  dtsLossless: 2000,
-  mp3: -1000,
-  truehd: 2000,
-
-  // Extras
-  threeD: -10000,
-  converted: -1000,
-  documentary: -250,
-  dubbed: -1000,
-  edition: 100,
-  proper: 20,
-  repack: 20,
-  site: -10000,
-  upscaled: -10000,
-
-  // Trash
-  cam: -10000,
-  cleanAudio: -10000,
-  r5: -10000,
-  satrip: -10000,
-  screener: -10000,
-  size: -10000,
-  telecine: -10000,
-  telesync: -10000,
-});
+export const createRankingModel = (
+  input: z.input<typeof RankingModelSchema> = {},
+) => RankingModelSchema.decode(input);
